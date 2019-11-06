4860e4503e3393a6846a9cd95b91e88276ecfb43
Eric Andrews <eric.barclay.andrews@gmail.com>
2019-11-06 12:54:06 -0500

    basic recursive solver done; still need to add logic for if simple elimination fails

--- python/solver.py
+++ python/solver.py
@@ -19,6 +19,92 @@ def printBoard(board):
                 print("| ", end = "")
             print(str(board[i][j]) + " ", end = "")
         print()
-    print()
+    return
 
-printBoard(parse("board0.txt"))
+# returns all numbers in the given row
+def inRow(board, row):
+    return [i for i in board[row] if i != 0]
+
+# returns all numbers in the given column
+def inCol(board, col):
+    return [board[i][col] for i in range(9) if board[i][col] != 0]
+
+# returns all numbers in the square around x, y
+def inSquare(board, row, col):
+    # starting indexes of the square (top left corner)
+    rowStart = (row // 3) * 3
+    colStart = (col // 3) * 3
+    ret = []
+    for r in range(rowStart, rowStart + 3):
+        for c in  range(colStart, colStart + 3):
+            if board[r][c] != 0:
+                ret.append(board[r][c])
+    return ret
+    
+def squareOpts(board, row, col):
+    # square already filled; no work to do
+    if (board[row][col] != 0):
+        return [board[row][col]]
+
+    # generate all options by process of elimination
+    opts = list(range(1, 10))
+    for num in inRow(board, row):
+        if num in opts:
+            opts.remove(num)
+    for num in inCol(board, col):
+        if num in opts:
+            opts.remove(num)
+    for num in inSquare(board, row, col):
+        if num in opts:
+            opts.remove(num)
+    return opts
+
+# generates a 3d array. opts[i][j] represents all possible numbers for square
+# [i][j] in the board.
+def genOpts(board):
+    opts = []
+    for r in range(9):
+        row = []
+        for c in range(9):
+            row.append(squareOpts(board, r, c))
+        opts.append(row)
+    return opts
+    
+# solves the board by filling in all squares with one logically possible
+# answer. if none of those squares exist, tries every option for the square
+# with the fewest options. returns False if the board is self-contradictory,
+# returns the solved board otherwise.
+def solve(board):
+    options = genOpts(board)
+    newBoard = []
+    solved = True
+    for r in range(9):
+        newRow = []
+        for c in range(9):
+            # no option exists; the board is impossible
+            if (options[r][c] == []):
+                return False
+            if (len(options[r][c]) == 1):
+                newRow.append(options[r][c][0])
+            else:
+                solved = False
+                newRow.append(0)
+        newBoard.append(newRow)
+    # no unsolved squares left; we're done, so return the board
+    if solved:
+        return newBoard
+    # recurse!
+    else:
+        return solve(newBoard)
+
+curBoard = parse("board0.txt")
+printBoard(curBoard)
+printBoard(solve(curBoard))
+# print(inRow(curBoard, 2))
+# print(inRow(curBoard, 7))
+# print(inCol(curBoard, 4))
+# print(inCol(curBoard, 8))
+# print(inSquare(curBoard, 0, 0))
+# print(inSquare(curBoard, 4, 4))
+# print(inSquare(curBoard, 5, 5))
+# print(inSquare(curBoard, 0, 8))
